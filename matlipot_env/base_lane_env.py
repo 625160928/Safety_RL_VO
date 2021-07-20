6dbd4fba5a72236eb85287f3b3a06649f7a5f604
625160928 <1007628071@qq.com>
2021-07-20 09:44:04 +0800

    添加注释

--- matlipot_env/base_lane_env.py
+++ matlipot_env/base_lane_env.py
@@ -6,27 +6,44 @@ from matplotlib.ticker import MultipleLocator
 
 from npc_car.npc_car import NPCCar
 
-
+#基本变道环境类
 class ChangeLaneEnv():
+    #model 为车辆模型，详情参考simulate car model
+    #init lane 是车辆出生点车道
+    #seed 随机数种子
     def __init__(self,model,init_lane=0,seed=0):
+        #车道数量
         self._lane_number=3
+        #单车道宽度
         self._lane_length=3
+        #npc车辆列表，所有在环境里面正在迭代更新的车辆都会在这个数组里
+        #npc列表里存的是 NPCCar 类
         self._npc_car_list=[]
-        self._obs_range=10
+        #观察范围
+        self._obs_range=30
+        #时间戳
         self._time=0
+        #随机数种子，如果之后需要复现场景可以通过随机数种子重复试验
         self._random_seed=seed
+        #时间步长
         self._step=0.1
+        #计划列表
+        #存的是 PreCreateCar 类
         self._create_plan=[]
+        #仿真时间上限
         self._simulate_max_time=500
-        random.seed=self._random_seed
+        #设置随机数种子
+        random.seed(self._random_seed)
+        #设置车辆模型
         self._player_model=model
+        #设置车辆位置
         self._player_model.set_position(init_lane * self._lane_length + self._lane_length / 2, 0, math.pi/2)
 
     def draw_env(self,keep=False):
         plt.clf()
         # plt.figure(dpi=70, figsize=(self._lane_number*self._lane_length, self._obs_range*2))
         x_major_locator = MultipleLocator(1)
-        y_major_locator = MultipleLocator(1)
+        y_major_locator = MultipleLocator(10)
         ax = plt.gca()
         # ax为两条坐标轴的实例
         ax.xaxis.set_major_locator(x_major_locator)
@@ -34,18 +51,24 @@ class ChangeLaneEnv():
         ax.yaxis.set_major_locator(y_major_locator)
         #通过玩家位置设置绘图区域
         _,player_position_y,_,_,_=self._player_model.get_infomation()
+        #设置显示范围
         plt.axis([0,self._lane_number*self._lane_length, player_position_y-self._obs_range, player_position_y+self._obs_range])
+        #绘制玩家车辆
         self._player_model.draw_car(color='red')
+        #绘制车道线
         for i in range(0,self._lane_number):
             plt.plot([i*self._lane_length,i*self._lane_length],[player_position_y-self._obs_range, player_position_y+self._obs_range],color='grey')
+        #绘制npc车辆
         for car in self._npc_car_list:
-            car.model.draw_car()
+            car.model.draw_car(color='black')
+        #显示方法
         if keep==False:
             plt.pause(0.1)
         else:
             plt.show()
 
-    #将计划中的
+    #将计划里应该出现的车辆添加至npc车辆列表
+    #添加计划里的车辆
     def _update_create_npc_car(self):
         for plan in self._create_plan:
             if self._time > plan.create_time:
@@ -70,6 +93,8 @@ class ChangeLaneEnv():
             v, w = npc_car.strategy.get_control(self._time, obs)
             npc_car.model.update(v, w)
 
+    #观察周围环境
+    #这部分需要根据实际情况进行更改，需要讨论
     def obs(self):
         player_position_x,player_position_y,player_position_theta,v,w=self._player_model.get_infomation()
         return self._time,player_position_x,player_position_y,player_position_theta,v,w
--- matlipot_env/one_lane_empty_env.py
+++ matlipot_env/one_lane_empty_env.py
@@ -8,8 +8,18 @@ from npc_car.npc_car_strategy.strategy_speed_down_and_stay import StrategySpeedD
 from npc_car.pre_create_car import  PreCreateCar
 from npc_car.car_model import simulate_car_model
 from npc_car.npc_car import NPCCar
+"""
+OneLaneEmptyEnv(ChangeLaneEnv)
+该类为除了empty lane 没有车辆，其他车道都在某个时间点附近刷新一部车辆
+刷新的npc车辆会在玩家车辆的前方
+车辆运行策略为先低速运行一段时间保证拉近与玩家车辆的距离，随后恢复高速匀速运动
 
-class DirEnv(ChangeLaneEnv):
+"""
+class OneLaneEmptyEnv(ChangeLaneEnv):
+    # model 为车辆模型，详情参考simulate car model
+    # init lane 是车辆出生点车道
+    #empty lane 是空车道的位置
+    # seed 随机数种子
     def __init__(self,model,init_lane=0,empty_lane=0,seed=0):
         ChangeLaneEnv.__init__(self,model,init_lane,seed)
 
@@ -55,7 +65,7 @@ class DirEnv(ChangeLaneEnv):
 
 if __name__ == "__main__":
     player_model= simulate_car_model.create_car_example()
-    env=DirEnv(model=player_model,init_lane=0,empty_lane=1,seed=10)
+    env=OneLaneEmptyEnv(model=player_model, init_lane=0, empty_lane=1, seed=10)
 
     for i in range(1000):
         env.update(50,0)
