dadd3d6dbfb88a14f6b1a78af609791a4a8c03ef
625160928 <1007628071@qq.com>
2021-07-20 09:51:11 +0800

    添加注释

--- matlipot_env/base_lane_env.py
+++ matlipot_env/base_lane_env.py
@@ -27,16 +27,16 @@ class ChangeLaneEnv():
         self._random_seed=seed
         #时间步长
         self._step=0.1
-        #计划列表
+        #npc计划列表
         #存的是 PreCreateCar 类
         self._create_plan=[]
         #仿真时间上限
         self._simulate_max_time=500
         #设置随机数种子
         random.seed(self._random_seed)
-        #设置车辆模型
+        #设置玩家车辆模型
         self._player_model=model
-        #设置车辆位置
+        #设置玩家车辆位置
         self._player_model.set_position(init_lane * self._lane_length + self._lane_length / 2, 0, math.pi/2)
 
     def draw_env(self,keep=False):
@@ -71,12 +71,15 @@ class ChangeLaneEnv():
     #添加计划里的车辆
     def _update_create_npc_car(self):
         for plan in self._create_plan:
+            #只有达到预定时间的npc车辆才会被创建
             if self._time > plan.create_time:
+                #设置npc策略
                 strategy = plan.strategy
-                # 设置仿真模型
+                # 设置npc仿真模型
                 car_model = plan.car_model
+                #通过当前玩家的位置设置npc的刷新位置
                 _, player_position_y, _, _, _ = self._player_model.get_infomation()
-                # 在仿真模型中设置车辆位置
+                # 在仿真模型中设置npc车辆位置
                 car_model.set_position(plan.lane * self._lane_length + self._lane_length / 2,
                                        player_position_y + self._obs_range + 10, math.pi / 2)
                 # 设置npc车辆
@@ -93,11 +96,15 @@ class ChangeLaneEnv():
             v, w = npc_car.strategy.get_control(self._time, obs)
             npc_car.model.update(v, w)
 
-
+    #迭代
     def update(self,v,w):
+        #更新时间戳
         self._time+=self._step
+        #根据时间戳创建计划车辆
         self._update_create_npc_car()
+        #更新npc车辆的位置
         self._update_npc_car()
+        #更新玩家的位置
         self._player_model.update(v, w)
 
     #观察周围环境
